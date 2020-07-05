4c96983a3a6e7bc48441018402ccfe668b900a37
mmassetti <matiasmassetti@gmail.com>
2020-07-05 18:56:08 -0300

    working with blogs logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Blogs from "../components/Blogs"
 export default ({ data }) => {
   const {
     allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
   } = data
 
   return (
@@ -17,6 +18,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="blog" showLink />
     </Layout>
   )
 }
@@ -45,6 +47,24 @@ export const query = graphql`
           title
         }
         order
+      }
+    }
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        slug
+        content
+        description
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
