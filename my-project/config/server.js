fa6be2c1f524c5cc5866a1091b94e8cca1178112
Matias Massetti <matiasmassetti@gmail.com>
2021-07-01 17:27:17 -0300

    remove unused folder

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6a0e6072809b20ec1e0a5ea5425b2256'),
-    },
-  },
-});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f853808a-3123-4412-a60f-f4e96d3aeae3'
-};
